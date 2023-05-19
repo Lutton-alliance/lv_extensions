7cc5c7aba6c447ecd9ffe7b418701fa2b2f70d4a
Lutton <82480124+Lutton-alliance@users.noreply.github.com>
2023-05-19 18:08:17 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Alliance Development'
+description 'lv_extensions | Las Venturas Project Addition'
+version '0.1.0'
+
+this_is_a_map 'yes'
+
+dependencies {
+    'lv_fortcarson'
+}
